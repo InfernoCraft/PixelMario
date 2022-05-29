9d64d0ade0c6639fef3a20e7a26fb8e46031ccb7
BuildTools <unconfigured@null.spigotmc.org>
2022-05-29 19:52:03 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import jade.Window;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Window window = Window.get();
+            window.run();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ src/main/java/jade/Scene.java
@@ -0,0 +1,35 @@
+package jade;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Scene {
+
+    protected Camera camera;
+    private boolean isRunning = false;
+    private List<GameObject> gameObjects = new ArrayList<>();
+
+    public Scene() {
+
+    }
+
+    public void init() {
+
+    }
+
+    public void start() {
+        for (GameObject go : gameObjects) {
+            go.start();
+        }
+    }
+
+    public void addGameObjectToScene() {
+        if (!isRunning) {
+            
+        }
+    }
+
+    public abstract void update(float dt);
+
+
+}
